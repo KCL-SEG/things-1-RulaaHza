86e01cc69b102d4a939dc5d5742e77e27867523d
Rulaa Alhazzani <q.rxr@outlook.sa>
2023-12-07 18:35:23 +0000

    fixed some issues

--- things/models.py
+++ things/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
-class Thing(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
+# Create your models here.
