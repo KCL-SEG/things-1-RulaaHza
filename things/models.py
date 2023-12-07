90fcef167d51a3f4c78f0fcac65dd45358a0bf99
Rulaa Alhazzani <q.rxr@outlook.sa>
2023-12-07 20:17:56 +0000

    fixed models second test

--- things/models.py
+++ things/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255, default='Default Description')
+    quantity = models.IntegerField(default=0)  # Set the default value
 
     def __str__(self):
         return self.name
