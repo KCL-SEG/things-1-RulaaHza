990ad56455040c9e376c98fbac67de9230d1f060
Rulaa Alhazzani <q.rxr@outlook.sa>
2023-12-07 20:23:12 +0000

    fixed models second test

--- things/models.py
+++ things/models.py
@@ -1,9 +1,14 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
+def validate_quantity_range(value):
+    if value < 0 or value > 100:
+        raise ValidationError('Quantity must be between 0 and 100 (inclusive).')
+
 class Thing(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=255, default='Default Description')
-    quantity = models.IntegerField(default=0)  # Set the default value
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, blank=True)
+    quantity = models.IntegerField(validators=[validate_quantity_range])
 
     def __str__(self):
         return self.name
